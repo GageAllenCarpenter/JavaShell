5f9b36295e2b0dcdf1bb69f5c18233660e2bda79
gcarp12 <gcarp12@WGU.edu>
2023-08-25 00:46:23 -0400

    brought src up to root. Will need to update pipeline with new path

--- JavaShell/src/main/java/com/github/gageallencarpenter/App.java
+++
@@ -1,7 +0,0 @@
-package com.github.gageallencarpenter;
-
-public class App {
-    public static void main(String[] args) {
-        new Powershell().execute("ipconfig /all").forEach(System.out::println);
-    }
-}
